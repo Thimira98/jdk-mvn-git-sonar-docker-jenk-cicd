184781e6932984219d62f80eba888f231da9dd1b
Dr. Kasun Jinasena <kasunkosala@yahoo.com>
2024-02-16 23:45:08 +0530

    Update App.java
    new Car()

--- src/main/java/sjp/msc/cs/App.java
+++ src/main/java/sjp/msc/cs/App.java
@@ -16,6 +16,7 @@ public class App
 {        
     public static void main( String[] args )throws IOException {
         System.out.println( "Hello World!" );
+        System.out.println( new Car() );
 
         HttpServer server = HttpServer.create(new InetSocketAddress(8082), 0);
         server.createContext("/", new MyHandler());
