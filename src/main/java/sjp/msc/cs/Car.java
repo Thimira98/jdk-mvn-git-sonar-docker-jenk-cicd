a2fbfb011057f3edda76e5ff0c46aadf20c8fb9d
Dr. Kasun Jinasena <kasunkosala@yahoo.com>
2024-02-16 23:44:13 +0530

    Create Car.java
    Car.java

---
+++ src/main/java/sjp/msc/cs/Car.java
@@ -0,0 +1,16 @@
+public class Car
+{
+  private String model;
+  private double price;
+
+  public Car()
+  {
+    model = "Honda CR-V";
+    setPrice(124);  
+  }
+  public void setPrice(int val)
+  {
+    price = val;
+  }
+}
+
